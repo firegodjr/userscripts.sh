9044272739246bb702016b06d506f8e84f2c7956
firegodjr <firegodjr.es@gmail.com>
2026-03-07 00:27:02 -0600

    Cleanup

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -1,24 +1,21 @@
 
+# Atomic
 # Distrobox
 alias dbe="distrobox enter"
 alias dbc="distrobox create -i devbase --name"
 alias dbl="distrobox list"
 alias db="distrobox"
+# Ostree
+alias os="ostree"
+alias ros="rpm-ostree"
 
 # Neovim
 alias v="nvim"
 alias va="pushd ~/.aliases;nvim ./bash_aliases.sh;popd;source ~/.aliases/bash_aliases.sh"
-NVIM_CONFIG="$HOME/.config/nvim"
 alias nvim-update="pushd $NVIM_CONFIG; git pull; popd"
 alias nvim-update-force="pushd $NVIM_CONFIG; git reset --hard HEAD; git pull; popd"
 
-alias neovide="flatpak run dev.neovide.neovide"
+# Utilities
 alias lg="lazygit"
 alias ldr="lazydocker"
-. "$HOME/.cargo/env"
 
-# Backups
-alias backup="systemctl --user start backup.service"
-alias sdrl="systemctl --user daemon-reload"
-
-alias spfy="spotify_player"
